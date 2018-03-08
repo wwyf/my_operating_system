952ee05a75c744ce69248ed903c930bd9b8697ab
walker <WalkerYF@yeah.net>
2018-03-08 18:24:27 +0800

    emmmmm

--- t1/t1.asm
+++ t1/t1.asm
@@ -1,4 +1,9 @@
-org 07c00h
+%ifdef DEBUG
+    org 0100h
+%else
+    org 07c00h
+%endif
+
 mov ax, cs
 mov ds, ax
 mov es, ax
@@ -8,13 +13,13 @@ jmp $
 DispStr:
 mov ax, BootMessage
 mov bp, ax
-mov cx, 16
+mov cx, 22
 mov ax, 01301h
 mov bx, 000ch
 mov dl, 0
 int 10h
 ret
 
-BootMessage: db "Hellp, OS world!"
+BootMessage: db "Hello, OS world of wyf!"
 times 510-($-$$) db 0
 dw 0xaa55
---
+++ t1/update_img.sh
@@ -0,0 +1,21 @@
+name=$1
+nasm -f $1.asm -o $1.bin
+# add the feature : if the image not exist, create the image
+if [ ! -f "a.img" ]
+then
+    echo "a.img not exist!"
+    echo "use bximage to create a image";
+    exit
+else
+    echo "a.img exists !"
+fi
+dd if=$1.bin of=a.img bs=512 count=1 conv=notrunc
+if [ ! -f "bochsrc.bxrc" ]
+then
+    echo "bochsrc.bxrc not exist!"
+    echo "copy a bochsrc.bxrc to this path quickly";
+    exit
+else
+    echo "bochsrc.bxrc exists !"
+fi
+bochs -q
