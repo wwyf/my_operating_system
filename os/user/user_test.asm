1c71ff3501c47da6a10e78eafa31be37373488a2
walker <WalkerYF@yeah.net>
2018-03-31 12:50:18 +0800

    edit user_test.asm

--- os/user/user_test.asm
+++ os/user/user_test.asm
@@ -2,27 +2,24 @@ section my_user1_program_header vstart=0x10000
 
 code_start:
 	; 初始化用户程序段地址
-    ; 此时ds指向header
-    ; cs 指向正确的位置。
-    ; mov ax, ds
-    ; mov es, ax
-    ; ; es 指向header
-    ; mov ax, [stack_segment]
-    ; mov ss, ax
-    ; mov sp, stack_end
-    ; mov ax, [data_segment]
-    ; mov ds, ax
+    mov ax, cs
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7FF
+
 	mov ax, 1301h		 ; AH = 13h（功能号）、AL = 01h（光标置于串尾）
 	mov bx, 0007h		 ; 页号为0(BH = 0) 黑底白字(BL = 07h)
 	mov dl, 0 		 ; 列号=0
 	mov dh, 2		       ; 行号=0
-	mov cx, user1_MessageLength  ; CX = 串长（=9）
-	mov bp, user1_Message		 ; es:BP=当前串的偏移地址
+	mov cx, user_messageLength  ; CX = 串长（=9）
+	mov bp, user_message		 ; es:BP=当前串的偏移地址
 	int 10h			 ; BIOS的10h功能：显示一行字符
     mov ax, 0b800h
     mov es, ax
     mov ax, 0702h
     mov [es:0x00], ax
+
 return_point:
     mov ah, 01h
     int 16h
@@ -35,11 +32,12 @@ return_point:
 
     cmp al, 'q' ; 如果键入q则退出
     jnz return_point
+    mov ax, 0x4c00
 	int 40h
 
-user1_Message:
+user_message:
     db '   You are in my test program now!'
     db 0x0d
     db 0x0a
     db '   Enter q to return system menu!'
-user1_MessageLength equ ($-user1_Message)
+user_mssageLength equ ($-user_message)
