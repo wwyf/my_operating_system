e1c431c1076e80902d7f4a3de882b75e70502cf2
walker <WalkerYF@yeah.net>
2018-04-15 20:59:50 +0800

    add proc.h

--- os/include/global.h
+++ os/include/global.h
@@ -5,5 +5,13 @@
 
 #include "../include/type.h"
 #define MAX_SYSTEM_CALL 256
+#define MAX_PROCESS_NUM 256
 
+// 系统调用表
 EXTERN u16 system_call[MAX_SYSTEM_CALL];
+
+// 进程控制块表
+EXTERN PCB_t process_block[MAX_SYSTEM_CALL];
+
+// 当前运行进程号（对应进程控制块表）
+EXTERN u16 current_process;
