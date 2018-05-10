daa368442e736b6d10ac820d1cbe9d45ea56a981
walker <WalkerYF@yeah.net>
2018-05-10 09:14:42 +0800

    增加了初始化8259A中断控制器的代码

--- wyf-os/boot/boot.asm
+++ wyf-os/boot/boot.asm
@@ -204,7 +204,46 @@ end_move:
 
 ;----------------------初始化8259A中断控制器--------------------------
 
-TODO:
+Init8259A:
+	mov	al, 011h
+	out	020h, al	; 主8259, ICW1.
+	call	io_delay
+
+	out	0A0h, al	; 从8259, ICW1.
+	call	io_delay
+
+	mov	al, 020h	; IRQ0 对应中断向量 0x20
+	out	021h, al	; 主8259, ICW2.
+	call	io_delay
+
+	mov	al, 028h	; IRQ8 对应中断向量 0x28
+	out	0A1h, al	; 从8259, ICW2.
+	call	io_delay
+
+	mov	al, 004h	; IR2 对应从8259
+	out	021h, al	; 主8259, ICW3.
+	call	io_delay
+
+	mov	al, 002h	; 对应主8259的 IR2
+	out	0A1h, al	; 从8259, ICW3.
+	call	io_delay
+
+	mov	al, 001h
+	out	021h, al	; 主8259, ICW4.
+	call	io_delay
+
+	out	0A1h, al	; 从8259, ICW4.
+	call	io_delay
+
+	;mov	al, 11111111b	; 屏蔽主8259所有中断
+	mov	al, 11111110b	; 仅仅开启定时器中断
+	out	021h, al	; 主8259, OCW1.
+	call	io_delay
+
+	mov	al, 11111111b	; 屏蔽从8259所有中断
+	out	0A1h, al	; 从8259, OCW1.
+	call	io_delay
+
 
 ;------------------------设置保护模式比特位--------------------------
 
@@ -220,6 +259,13 @@ TODO:
     jmp	dword SelectorKernel:0
 ;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+io_delay:
+	nop
+	nop
+	nop
+	nop
+	ret
 
 
 ;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
